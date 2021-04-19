fd0eef9eb0917655db6889a062fa900fd6900aa8
luvenable <gderko@mail.ru>
2021-04-19 11:51:17 +0300

    ClassFile

--- ConsoleApp1Prakt1/ConsoleApp1Prakt1/Class1.cs
+++ ConsoleApp1Prakt1/ConsoleApp1Prakt1/Class1.cs
@@ -6,5 +6,8 @@ namespace ConsoleApp1Prakt1
 {
     class Class1
     {
+        public string nazvanie { get; set; }
+        public float kalibr { get; set; }
+        public double dalnost { get; set; }
     }
 }
