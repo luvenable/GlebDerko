e9bc2842e8ccfe20fb0f14be68aaefe32da6ca12
luvenable <gderko@mail.ru>
2021-04-19 11:58:16 +0300

    atributi

--- ConsoleApp1Prakt1/ConsoleApp1Prakt1/Class1.cs
+++ ConsoleApp1Prakt1/ConsoleApp1Prakt1/Class1.cs
@@ -1,13 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1Prakt1
+namespace ConsoleApp2
 {
     class Class1
     {
+        static Class1[] a = new Class1[1000];
+        static int n;
+
         public string nazvanie { get; set; }
         public float kalibr { get; set; }
         public double dalnost { get; set; }
+
+        internal void add()
+        {
+            Console.WriteLine();
+            Console.Write("Введите кол-во записей: ");
+            n = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new Class1();
+                Console.Write("Название: ");
+                a[i].nazvanie = Console.ReadLine();
+                Console.Write("Калибр: ");
+                a[i].kalibr = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Дальность: ");
+                a[i].dalnost = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+
+        internal void print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Список стрелковых оружий:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Название:" + a[i].nazvanie);
+                Console.WriteLine("Калибр:" + a[i].kalibr);
+                Console.WriteLine("Дальность:" + a[i].dalnost);
+
+            }
+            Console.WriteLine();
+        }
     }
 }
